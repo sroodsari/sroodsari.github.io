4195dcdf916cf15e38fe39135a4408b707cc5eb4
Susan Roodsari <sroodsari@berkeley.edu>
2024-03-26 19:41:31 -0700

    initial commit for personal site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-xl justify-center">
+      <p className="text-md font-bold text-red-600">
+        I'm Susan and this is my website! blah blah blah
+      </p>
     </div>
   );
 }
